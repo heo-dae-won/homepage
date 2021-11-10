53dbc6ae58bbdd5eb867a0055d40311f85f2a8a4
heo-dae-won <heodaewon92@kakao.com>
2021-11-11 01:44:51 +0900

    update src

---
+++ src/main/java/com/heodae/homepage/home/HomeController.java
@@ -0,0 +1,16 @@
+package com.heodae.homepage.home;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller("/")
+public class HomeController {
+
+    @GetMapping
+    public String home(Model model){
+        System.out.println("home controller");
+        return "index";
+    }
+}
