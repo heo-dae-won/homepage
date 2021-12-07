edf0136336c592bdd4e8f606e976ec5e00f82ff8
heo-dae-won <heodaewon92@kakao.com>
2021-12-07 16:20:47 +0900

    update pom.xml

--- src/main/java/com/heodae/homepage/home/HomeController.java
+++ src/main/java/com/heodae/homepage/home/HomeController.java
@@ -1,15 +1,9 @@
 package com.heodae.homepage.home;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Optional;
 
 @Controller("/")
 public class HomeController {
@@ -18,12 +12,5 @@ public class HomeController {
     public String home(Model model){
         System.out.println("home controller");
         return "index";
-    }
-
-    @PatchMapping
-    public ResponseEntity<String> homet(){
-        String st = "etst";
-
-        return new ResponseEntity<>(st, HttpStatus.OK);
     }
 }
