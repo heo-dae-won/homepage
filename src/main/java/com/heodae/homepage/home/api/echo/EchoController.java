31297b4a32183ef39e42b8171858b96d0865822b
heo-dae-won <heodaewon92@kakao.com>
2022-03-01 17:19:01 +0900

    Update EchoController.java

--- src/main/java/com/heodae/homepage/home/api/echo/EchoController.java
+++ src/main/java/com/heodae/homepage/home/api/echo/EchoController.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +16,7 @@ import java.net.http.HttpResponse;
 public class EchoController {
 
 
-    @RequestMapping("/{str}")
+    @GetMapping("/{str}")
     public ResponseEntity<String> getEcho(@PathVariable String str){
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
